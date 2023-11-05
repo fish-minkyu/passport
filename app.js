3a688f5409c72fb83ce3da18be818a8c73ee5736
fish-minkyu <e951219@naver.com>
2023-11-05 15:01:42 +0900

    update: code

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const app = express();
+const passport = require('passport')
+
+
+app.listen(1219, () => {
+  console.log(' Passport Project is working on port 1219 ')
+})
